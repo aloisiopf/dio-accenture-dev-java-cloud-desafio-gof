cc7dd75ccad8012002356f1b669d6b2f3aab22fc
aloisiopf <aloisiopf@gmail.com>
2026-03-10 17:35:41 -0300

    Criação da estrutura base do projeto BookTracker (Sistema de Gerenciamento de Livros de Leitura) para aplicar os conhecimentos aprendidos sobre Design Pattern. Foi utilizado o Spring Initializr usando Java 21, Maven, H2 Database, Lombok, JPA, Validation, JPA

---
+++ booktracker/src/test/java/br/eti/aloisio/dio/accenture/booktracker/BooktrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.eti.aloisio.dio.accenture.booktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
